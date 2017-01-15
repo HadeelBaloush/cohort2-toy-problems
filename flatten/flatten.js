88292b9e1e6610d4d375c754b9b81d8179acf5d9
HadeelBaloush <hadeelbaloush@gmail.com>
2017-01-15 11:11:27 +0400

    flatten

--- flatten/flatten.js
+++ flatten/flatten.js
@@ -12,6 +12,22 @@ flatten('a', ['b', 2], 3, null, [[4], ['c']]) // returns ['a', 'b', 2, 3, null, 
 
 */
 
+var res = [];
 function flatten(){
-
+	var arg = Array.from(arguments);
+	
+	for (var i = 0; i < arguments.length; i++) {
+		
+		if(typeof arg[i] === "number" || typeof arg[i] === "string" || arg[i] === null || arg[i] === undefined ){
+			res.push(arg[i]);
+		}
+		else if(Array.isArray(arg[i])){
+			var arr = [];
+			for (var j = 0; j < arg[i].length; j++) {
+				arr.push(flatten(arg[i][j]));
+			}
+			res.concat(arr);
+		}
+	}
+	return res;
 }
