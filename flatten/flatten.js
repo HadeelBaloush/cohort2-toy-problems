775e668d2555aaa351bfd667876acddebe74579d
HadeelBaloush <hadeelbaloush@gmail.com>
2017-01-16 11:10:07 +0400

    Queue 2 stacks

--- flatten/flatten.js
+++ flatten/flatten.js
@@ -15,7 +15,6 @@ flatten('a', ['b', 2], 3, null, [[4], ['c']]) // returns ['a', 'b', 2, 3, null, 
 var res = [];
 function flatten(){
 	var arg = Array.from(arguments);
-	
 	for (var i = 0; i < arguments.length; i++) {
 		
 		if(typeof arg[i] === "number" || typeof arg[i] === "string" || arg[i] === null || arg[i] === undefined ){
--- queueStack/queueStack.js
+++ queueStack/queueStack.js
@@ -6,42 +6,51 @@
  /**
    * Stack Class
    */
- var Stack = function() {
-  this.stack=[];
+   var Stack = function() {
+    this.stack=[];
 
    // add an item to the top of the stack
    this.push = function(value){
-   };
+    this.stack.push(value);
+    return this.stack;
+  };
 
    // remove an item from the top of the stack
    this.pop = function(){
-   };
+    return this.stack.pop();
+  };
 
    // return the number of items in the stack
    this.size = function(){
-   };
- };
+    return this.stack.length;
+  };
+}
 
  /**
    * Queue Class
    */
- var Queue = function() {
+   var Queue = function() {
    // Use two `stack` instances to implement your `queue` Class
    var inbox = new Stack();
    var outbox = new Stack();
 
    // called to add an item to the `queue`
    this.enqueue = function(value){
-     
-     }
-   };
+    inbox.push(value);
+  };
 
    // called to remove an item from the `queue`
    this.dequeue = function(){
+    if(!outbox.size()){
+      while(inbox.size()){
+        outbox.push(inbox.pop());
+      }
     }
-   };
+    return outbox.pop();
+  };
 
    // should return the number of items in the queue
    this.size = function(){
+    return inbox.size()+outbox.size();
    };
- };
+ }
