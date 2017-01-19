302f437044c7c16190be32b5faa304af2aee8f81
HadeelBaloush <hadeelbaloush@gmail.com>
2017-01-19 11:01:04 +0400

    linked list

--- linkedList/linkedList.js
+++ linkedList/linkedList.js
@@ -18,7 +18,50 @@
 
 
 var LinkedList = function(){
-  //fill me in!
+  this.head = null;
+  this.tail = null;
+  this.listLength = 0;
+
 };
 
 //write methods here!
+
+LinkedList.prototype.addToTail = function(value) {
+	var node = Node(value);
+	if(this.head === null){
+		this.head = node;
+		this.tail = node;
+		this.listLength++;
+	}
+	else{
+		this.tail.next = node;
+		this.tail = node;
+		this.listLength++;
+	}
+}
+
+LinkedList.prototype.removeHead = function() {
+	var headValue = this.head;
+	this.head = this.head.next;
+	this.listLength--;
+	return headValue.value;
+}
+
+
+LinkedList.prototype.contains = function(target){
+	var current = this.head;
+	for (var i = 0; i < this.listLength; i++) {
+		if(target === current.value){
+			return true;
+		}
+		current = current.next;
+	}
+	return false;
+}
+
+var Node = function(value){
+	return{
+		value: value,
+		next: null
+	};
+}
