d11cc72432443030d22b8d3f499982600cb7f44a
HadeelBaloush <hadeelbaloush@gmail.com>
2017-01-30 11:30:03 +0400

    compose & pipe

--- composePipe/composePipe.js
+++ composePipe/composePipe.js
@@ -32,25 +32,43 @@
   addAndMultiply(5);//should be 21
   var addAndMultiplyTwice = pipe(add2, multiplyBy3, multiplyBy3);
   addAndMultiplyTwice(5); //should be 63
- */
+  */
 
-'use strict';
-var add2 = function(number){ return number + 2; }
-var multiplyBy3 = function(number){ return number * 3; }
+  'use strict';
+  var add2 = function(number){ return number + 2; }
+  var multiplyBy3 = function(number){ return number * 3; }
 
-var pipe = function(func1,func2){
-  
-};
+  var pipe = function(func1,func2){
+    return function(x){
+      return func2(func1(x));
+    }
+  };
 
-var addAndMultiplyTwice = pipe(add2, multiplyBy3, multiplyBy3);
+  var addAndMultiplyTwice = pipe(add2, multiplyBy3, multiplyBy3);
 
 //==============================================================================
 
- var greet = function(name){ return 'hello ' + name;}
- var exclaim = function(statement) { return statement.toUpperCase() + '!';}
+var greet = function(name){ return 'hello ' + name;}
+var exclaim = function(statement) { return statement.toUpperCase() + '!';}
 
-var compose = function(){
-  
+
+// for more than two funcs 
+var compose = function(func1,func2){
+  var funcs = arguments;
+  return function(x){
+    var res = funcs[funcs.length - 1](x);
+    for (var i = funcs.length - 2; i >= 0; i--) {
+      res = funcs[i](res);
+    }
+    return res;
+  }
+};
+
+//for two funcs
+var compose = function(func1,func2){
+  return function(x){
+    return func1(func2(x))
+  }
 };
 
 
